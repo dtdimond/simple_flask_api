c9c148b08352fcafe2f7f766309e0e686fd3733f
Dan Dimond <dtdimond@gmail.com>
2021-06-19 21:09:00 -0400

    Bug fix with metric get route

--- flask_api/routes/metric.py
+++ flask_api/routes/metric.py
@@ -5,18 +5,20 @@ from .. import cache
 bp = Blueprint('metric', __name__)
 
 
-@bp.route('/metric/<string:metric_key>', methods=('GET', 'POST'))
-def register(metric_key):
-    if request.method == 'POST':
-        content = request.json
-        if "value" not in content:
-            return "Malformed POST data", 400
+@bp.route('/metric/<string:metric_key>', methods=('POST',))
+def metric_post(metric_key):
+    content = request.json
+    if "value" not in content:
+        return "Malformed POST data", 400
 
-        cache.cache_item(metric_key, content["value"])
-        return "OK", 200
-    else:  # GET
-        key_data = cache.get_from_cache(metric_key)
-        return jsonify(dict(value=sum_data(key_data)))
+    cache.cache_item(metric_key, content["value"])
+    return "OK", 200
+
+
+@bp.route('/metric/<string:metric_key>/sum', methods=('GET',))
+def metric_get(metric_key):
+    key_data = cache.get_from_cache(metric_key)
+    return jsonify(dict(value=sum_data(key_data)))
 
 
 def sum_data(key_data: list[tuple]) -> int:
--- flask_api/tests/test_metric.py
+++ flask_api/tests/test_metric.py
@@ -16,19 +16,19 @@ def test_metric_post_and_get_basic(mock_now, client, init_empty_cache):
     mock_now.return_value = now_ret_val
 
     # GET with nothing in the cache
-    response = client.get('/metric/some_key')
+    response = client.get('/metric/some_key/sum')
     assert response.status_code == 200
     assert response.json == dict(value=0)
 
     # POST some data, assert it gets properly rounded and stored for the GET
     response = client.post('/metric/some_key', json={"value": 10.1})
     assert response.status_code == 200
-    response = client.get('/metric/some_key')
+    response = client.get('/metric/some_key/sum')
     assert response.status_code == 200
     assert response.json == dict(value=10)
 
     # GET - other keys unaffected
-    response = client.get('/metric/some_other_key')
+    response = client.get('/metric/some_other_key/sum')
     assert response.status_code == 200
     assert response.json == dict(value=0)
 
@@ -40,22 +40,22 @@ def test_metric_post_and_get_expired_data(mock_now, client, init_empty_cache):
     # POST some data
     client.post('/metric/some_key', json={"value": 10.1})
     client.post('/metric/some_key', json={"value": -4.8})
-    response = client.get('/metric/some_key')
+    response = client.get('/metric/some_key/sum')
     assert response.json == dict(value=5)
 
     # Move time forward half an hour and re-check
     mock_now.return_value = datetime(2021, 6, 1, 10, 30)
-    response = client.get('/metric/some_key')
+    response = client.get('/metric/some_key/sum')
     assert response.json == dict(value=5)
 
     # Move time forward again 30 mins and re-check (edge case)
     mock_now.return_value = datetime(2021, 6, 1, 11, 0)
-    response = client.get('/metric/some_key')
+    response = client.get('/metric/some_key/sum')
     assert response.json == dict(value=5)
 
     # Move time forward again 1 more min and re-check
     mock_now.return_value = datetime(2021, 6, 1, 11, 1)
-    response = client.get('/metric/some_key')
+    response = client.get('/metric/some_key/sum')
     assert response.json == dict(value=0)
 
 
